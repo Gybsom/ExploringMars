276b84d54d641ea29e1d8bac5d28baab32664f18
Gybsom <gybsom.souza@gmail.com>
2021-07-27 20:46:08 -0300

    criação do projeto

---
+++ lib/exploringmars.ex
@@ -0,0 +1,22 @@
+defmodule Exploringmars do
+  @moduledoc """
+  Documentation for `Exploringmars`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exploringmars.hello()
+      :world
+
+  """
+  def main do
+    hello()
+  end
+
+  def hello do
+    :world
+  end
+end
---
+++ test/exploringmars_test.exs
@@ -0,0 +1,8 @@
+defmodule ExploringmarsTest do
+  use ExUnit.Case
+  doctest Exploringmars
+
+  test "greets the world" do
+    assert Exploringmars.hello() == :world
+  end
+end

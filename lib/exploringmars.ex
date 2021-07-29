1d3130e76e3a8447842d77935d0b7d12d86fdb42
Gybsom <gybsom.souza@gmail.com>
2021-07-29 05:09:33 -0300

    criadas as funções de leitura do txt com as coordenadas, rotação e movimentação

--- lib/exploringmars.ex
+++ lib/exploringmars.ex
@@ -1,22 +1,51 @@
 defmodule Exploringmars do
-  @moduledoc """
-  Documentation for `Exploringmars`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Exploringmars.hello()
-      :world
-
-  """
   def main do
-    hello()
+
+    IO.puts "Bem vindo ao programa de exploracao de Marte. Para este programa, vamos utilizar duas sondas (probe1 e probe2) e percorrer o perimetro de 8x8 com as coordenadas de navegacao predefinidas no arquivo \"coords.txt\".\n"
+    IO.puts "A primeira sonda inicia na posicao \"2 3 E\" e faz a movimentacao \"RMMMLMMLMRMLMRMM\"."
+    IO.puts "A segunda sonda inicia na posicao \"7 8 W\" e faz a movimentacao \"MMMLMLMRMMRMMMLM\"\n"
+    IO.gets "Pressione enter para visualizar a posicao final das sondas depois de percorrer as instrucoes.\n"
+
+
   end
 
-  def hello do
-    :world
+  def readCoordFile() do
+    {:ok, coordinates} = File.read("lib/coord.txt")
+    coordinates |> String.split("\r\n", trim: true) |> Enum.to_list
+  end
+
+  def rotate(direction, cardinalPosition) do
+    if direction == "L" do
+      case cardinalPosition do
+        "N" -> "W"
+        "W" -> "S"
+        "S" -> "E"
+        "E" -> "N"
+        _ -> "Valor invalido"
+      end
+    else
+      case cardinalPosition do
+        "N" -> "E"
+        "E" -> "S"
+        "S" -> "W"
+        "W" -> "N"
+        _ -> "Valor invalido"
+      end
+    end
+  end
+
+  def move(xaxis, yaxis, cardinalPosition) do
+    Enum.reduce(
+        [cardinalPosition],
+        {xaxis,yaxis},
+        fn x, {acc1, acc2} ->
+      cond do
+        x == "N" -> {acc1, acc2 + 1}
+        x == "E" -> {acc1 + 1, acc2}
+        x == "S" -> {acc1, acc2 - 1}
+        x == "W" -> {acc1 - 1, acc2}
+      end
+    end)
   end
 end
--- test/exploringmars_test.exs
+++ test/exploringmars_test.exs
@@ -2,7 +2,25 @@ defmodule ExploringmarsTest do
   use ExUnit.Case
   doctest Exploringmars
 
-  test "greets the world" do
-    assert Exploringmars.hello() == :world
+  #ExUnit.start()
+
+  test "Teste de leitura do arquivo de coordenadas" do
+    assert is_list(Exploringmars.readCoordFile()) == true
+  end
+
+  test "Teste de rotacao para a esquerda" do
+    assert Exploringmars.rotate("L", "N") == "W"
+  end
+
+  test "Teste de rotacao para a direita" do
+    assert Exploringmars.rotate("R", "N") == "E"
+  end
+
+  test "Teste de movimentacao horizontal (x)" do
+    assert Exploringmars.move(2, 3, "E") == {3,3}
+  end
+
+  test "Teste de movimentacao vertical (y)" do
+    assert Exploringmars.move(2, 3, "S") == {2,2}
   end
 end
